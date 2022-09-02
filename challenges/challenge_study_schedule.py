b23366312c2a42d7e3c4b04ccb06d8be84044f33
Matheus <matheusmoura2110@gmail.com>
2022-09-02 08:52:53 -0300

    task 02

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,13 +1,10 @@
 def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
-
     counter = 0
-
     for start, end in permanence_period:
         if not isinstance(start, int) or not isinstance(end, int):
             return None
-    
         if start <= target_time <= end:
             counter += 1
     return counter
