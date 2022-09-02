2782851e4b51e3e9310cae9277c2c9e47b676ced
Matheus <matheusmoura2110@gmail.com>
2022-09-02 08:59:04 -0300

    taks 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,14 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+    list1 = list(first_string.lower())
+    list2 = list(second_string.lower())
+
+    if len(list1) != len(list2):
+        return False
+
+
+    for i in list1:
+        if i in list2:
+            list2.remove(i)
+        else:
+            return False
+    return True
