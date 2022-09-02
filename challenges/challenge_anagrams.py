c3dcf8337eed484b637861d91d85c859e01680ab
Matheus <matheusmoura2110@gmail.com>
2022-09-02 09:02:04 -0300

    fix lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,11 +1,8 @@
 def is_anagram(first_string, second_string):
     list1 = list(first_string.lower())
     list2 = list(second_string.lower())
-
     if len(list1) != len(list2):
         return False
-
-
     for i in list1:
         if i in list2:
             list2.remove(i)
